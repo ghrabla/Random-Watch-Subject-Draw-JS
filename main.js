9097854547de1c62ce4b606d95e200e1ae5dd300
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-09-21 21:42:55 +0100

    just date

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-var alldata;
 fetch("./students.json")
 .then(response => response.json())
 .then(data => {
-    alldata = data;
-    console.log(data)
+    let arr = data;
+    console.log(arr)
+    localStorage.setItem("arrobj",JSON.stringify(arr))
 })
-
+ 
+// console.log()
 let day = new Date();
-localStorage.setItem("lastmonth",day.getMonth()*1+1)
+localStorage.setItem("lastmonth",day.getMonth()+1)
 localStorage.setItem("lastdate",day.getDate())
 localStorage.setItem("year",day.getFullYear())
+
 function remind(){
-    // console.log(alldata)
-    // document.writeln(JSON.stringify(alldata))
-    // let studentsarr = JSON.stringify(alldata);
-    // document.getElementById("table").innerHTML = studentsarr;
-    let day = new Date();
+   
+    let alldata = JSON.parse(localStorage.getItem("arrobj"));
+    
     let month = localStorage.getItem("lastmonth")*1;
     let lastday = localStorage.getItem("lastdate")*1+1;
     let year = localStorage.getItem("year")*1;
@@ -33,16 +33,56 @@ function remind(){
         
     }
     let pre_date = lastday +"/"+ month +"/"+ year;
-    let displaydat = JSON.stringify(alldata[Math.floor(alldata.length * Math.random())].name)+ " : " + pre_date;
-    document.getElementById("table").innerHTML = displaydat;
-    // let values = Object.values(alldata)
-    // console.log(values)
-    // const result = values.filter(alldata[Math.floor(alldata.length * Math.random())]);
-    // console.log(result); 
-    // alldata = alldata.filter(alldata[0])
+    
+    let randnum = Math.floor(alldata.length * Math.random());
+    let displaydat = JSON.stringify(alldata[randnum].name)+ " : " + pre_date;
+    // for (let index = 0; index < alldata.length; index++) {
+         
+    // }
+
+    
+
+        // Create an "li" node:
+        const node = document.createElement("li");
+        
+        // Create a text node:
+        const textnode = document.createTextNode(displaydat);
+        
+        // Append the text node to the "li" node:
+        node.appendChild(textnode);
+        
+        // Append the "li" node to the list:
+        document.getElementById("table").appendChild(node);
+        
+
+
+
+    // document.getElementById("table").innerHTML = displaydat;
+    
+    alldata = Object.values(alldata).filter((value , index) => {
+        // console.log(index)
+        return index != randnum;
+    });
+    console.log(alldata)
+    if(alldata.length==0){
+     
+          const node = document.createElement("h4");
+          const textnode = document.createTextNode("we sorted all the students thanks!");
+          node.appendChild(textnode);
+        document.getElementById("table").appendChild(node);
+        var element = document.getElementById("mybtn");
+        element.classList.toggle("hide-btn");
+    }
     // console.log(alldata)
+    localStorage.setItem("arrobj",JSON.stringify(alldata))
+    
+
+    
 
 }
+
+
+  
 
 
 
