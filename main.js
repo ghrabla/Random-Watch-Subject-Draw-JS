d6975073fbe3e5119348bf90aee79b98f83bd95f
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-09-23 11:58:42 +0100

    get last day

--- main.js
+++ main.js
@@ -6,10 +6,7 @@ fetch("http://localhost:3000/student")
     localStorage.setItem("arrobj",JSON.stringify(arr))
 })
  
-let day = new Date();
-localStorage.setItem("lastmonth",day.getMonth()+1)
-localStorage.setItem("lastdate",day.getDate())
-localStorage.setItem("year",day.getFullYear())
+
 
 
 
@@ -19,30 +16,15 @@ async function remind(){
    
     let alldata = JSON.parse(localStorage.getItem("arrobj"));
     
-    let month = localStorage.getItem("lastmonth")*1;
-    let lastday = localStorage.getItem("lastdate")*1+1;
-    let year = localStorage.getItem("year")*1;
-    localStorage.setItem("lastmonth", month);
-    localStorage.setItem("lastdate", lastday);
+   
 
-    if(lastday==23){
-        localStorage.setItem("lastdate", lastday+2);
-    }
-    if(lastday>30){
-        
-        localStorage.setItem("lastdate",0)
-        localStorage.setItem("lastmonth",month*1+1)
-    }
-    if(month==12 && lastday>30){
-        
-        localStorage.setItem("lastmonth",1);
-        localStorage.setItem("year",year*1+1);
-        
-    }
-    let pre_date = lastday +"/"+ month +"/"+ year;
+    
+     
+    // let pre_date = l;
+    let pre_date = "sorry no engless";
     
     let randnum = Math.floor(alldata.length * Math.random());
-    let displaydat = JSON.stringify(alldata[randnum].name)+ " : " + pre_date;
+    let displaydat = JSON.stringify(alldata[randnum].name)+ " : " ;
 //    const sendsort = await axios.post('http://localhost:3000/sortedstudent', { id: "", name: alldata[randnum].name , date:pre_date });
     // for (let index = 0; index < alldata.length; index++) {
          
@@ -58,10 +40,20 @@ async function remind(){
         
         // Append the text node to the "li" node:
         node.appendChild(textnode);
+        // Create an "li" node:
+        // const nodet = document.createElement("li");
+        
+        // Create a text node:
+        // const textnodet = document.createTextNode(pre_date);
+        
+        // Append the text node to the "li" node:
+        // node.appendChild(textnodet);
         
         // Append the "li" node to the list:
         document.getElementById("table").appendChild(node);
+        // document.getElementById("tdate").appendChild(nodet);
         
+
 
 
 
@@ -71,7 +63,7 @@ async function remind(){
         // console.log(index)
         return index != randnum;
     });
-    console.log(alldata)
+    // console.log(alldata)
     if(alldata.length==0){
      
           const node = document.createElement("h4");
---
+++ test.js
@@ -0,0 +1,66 @@
+var btn = document.getElementById("mybtn");
+
+function getDatesInRange(startDate) {
+  const date = new Date(startDate.getTime());
+  // array for pushed dates
+  const dates = [];
+
+  // put date in array and make a condition if this date in the array skipped
+
+  function printedName(tableName) {
+    let printedTable = tableName.join(" ");
+    // document.getElementById("tdate").innerHTML = printedTable;
+    const node = document.createElement("li");
+
+    const textnode = document.createTextNode(printedTable);
+
+     node.appendChild(textnode);
+
+     document.getElementById("tdate").appendChild(node);
+  }
+  // printedName(student,"allStudents")
+
+  btn.addEventListener("click", () => {
+    // get name of the day
+    var alldata = JSON.parse(localStorage.getItem("arrobj"));
+    var dayName = date.toString().split(" ")[0];
+
+    // make a condition to skipped the weekend
+    if (dayName != "Fri" && alldata.length > -1) {
+      // add new day after every pick
+      var test = date.setDate(date.getDate() + 1);
+      // change date to this format : yy-mm-dd
+      var ss = new Date(test);
+      var finalDate =
+        ss.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+      // push the date to an array called dates
+      dates.push(finalDate);
+      printedName(dates)
+      // document.getElementById("rDate").innerHTML = dates;
+    }
+    // if day day pass weekends day
+    else if (dayName == "Fri") {
+      var test = date.setDate(date.getDate() + 3);
+      // change date to this format : yy-mm-dd
+      var ss = new Date(test);
+      var finalDate =
+        ss.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+      // push the date to an array called dates
+      dates.push(finalDate);
+      printedName(dates)
+    } else {
+      // console.log("stop please");
+    }
+  });
+
+  return dates;
+}
+
+// 
+var today = new Date();
+var current_date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+const d1 = new Date(current_date);
+getDatesInRange(d1)
+
+
+
